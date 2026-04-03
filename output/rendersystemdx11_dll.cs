30fa46e746f5984caf1ae8783d238f605b76f13a
mnasibzade <munir.nasibzade@proton.me>
2026-04-03 18:29:16 +0400

    new update

--- output/rendersystemdx11_dll.cs
+++ output/rendersystemdx11_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-25 19:11:43.664891200 UTC
+// 2026-04-03 14:28:42.599611300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: rendersystemdx11.dll
